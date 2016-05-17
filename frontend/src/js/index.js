16cacc46009cd524d8ca7f9c05b4e9b3a3df0dc9
Yuumi Yoshida <yy@ey-office.com>
2016-05-17 14:40:55 +0900

    Score list

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,7 +1,79 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
+import fetch from 'isomorphic-fetch'
+
+class JyankeGamePage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {scores: []}
+  }
+  componentDidMount() {
+    this.getScores()
+  }
+  getScores() {
+    fetch('/jyankens.json')
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json)
+      this.setState({scores: json})
+    })
+    .catch((response) => console.log(response))
+  }
+  render() {
+    return (
+      <div>
+        <Header title="じゃんけん ポン！" />
+        <ScoreList scores={this.state.scores} />
+      </div>
+    )
+  }
+}
+
+const Header = (props) => (<h1>{props.title}</h1>)
+Header.propTypes = {
+  title: PropTypes.string
+}
+
+class ScoreList extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <td>時間</td><td>人間</td><td>コンピュータ</td><td>結果</td>
+          </tr>
+        </thead>
+        {this.props.scores.map((score) => <ScoreListItem  key={score.id} score={score} />)}
+       </table>
+    )
+  }
+}
+ScoreList.propTypes = {
+  scores: PropTypes.array
+}
+
+class ScoreListItem extends Component {
+  render() {
+    const teString = (te) => ["グー","チョキ", "パー"][te]
+    const judgmentString = (judgment) => ["引き分け","勝ち", "負け"][judgment]
+    const extractHHMM = (t) => t.substr(14, 5)
+    return (
+      <tbody>
+        <tr>
+          <td>{extractHHMM(this.props.score.created_at)}</td>
+          <td>{teString(this.props.score.human)}</td>
+          <td>{teString(this.props.score.computer)}</td>
+          <td>{judgmentString(this.props.score.judgment)}</td>
+        </tr>
+      </tbody>
+    )
+  }
+}
+ScoreListItem.propTypes = {
+  score: PropTypes.object
+}
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <JyankeGamePage />,
   document.getElementById('example')
 )
